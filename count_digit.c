2c55c007581a34bf5c715457da2fb18530d05002
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-05 09:19:44 +0530

    Update count_digit.c

--- count_digit.c
+++ count_digit.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 int main (){
+    // Taking input the number
     int n;
     printf ("enter the number:");
     scanf("%d",&n);
+    
+    // Counting the digits
     int count=0;
     while(n!=0){
         n=n/10;
