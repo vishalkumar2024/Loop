b8a9fad2483ed2d99cdd31a6e8ed8dc7bcf5665c
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-12 07:12:01 +0530

    Update printAP.c

--- printAP.c
+++ printAP.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main(){
+    // Taking input the no.
     int n;
     printf("enter the number :");
     scanf("%d",&n);
     int a=1;
+    // increment 'a' by three n times
     for(int i=1;i<=n;i++){
         printf("%d\n",a);
         a+=3;
