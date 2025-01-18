18ca4a700bc198e0430aa3acdf68a9cdfcc3e014
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-18 07:53:40 +0530

    Update printGP.c

--- printGP.c
+++ printGP.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main (){
+    // Taking input the number
     int n;
     printf("enter the number :");
     scanf("%d",&n);
     int a=1;
+    // increasing a by 2 times
     for(int i=1;i<=n;i++){
         printf("%d\n",a);
         a=a*2;
