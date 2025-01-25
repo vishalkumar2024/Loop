9c07009123f400683070ea3f42b34ff7876ef77e
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-25 07:32:45 +0530

    Update sum_num_reverse.c

--- sum_num_reverse.c
+++ sum_num_reverse.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main (){
   // Takng input the number
-  int n;
+  int num;
   printf("enter the number:");
-  scanf("%d",&n);
-  int s=n;
+  scanf("%d",&num);
+  int s=num;
    int r=0;
-   while(n!=0){
+   while(num!=0){
     r=r*10;
-    r=r+(n%10);
-    n=n/10;
+    r=r+(num%10);
+    num=num/10;
    } 
    printf("%d",s+r);
   return 0;
