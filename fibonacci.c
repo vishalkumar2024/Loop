ae914ba7549ccc90a24ef1679c7a34047b95b781
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-06-01 21:54:03 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -2,7 +2,7 @@
 int main (){
     // Taking input the number n
     int n;
-    printf("Enter the number :");
+    printf("Enter the number: ");
     scanf("%d",&n);
     
     // calculating the fibonacci
